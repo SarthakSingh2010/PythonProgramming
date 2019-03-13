45c66daa06e32c4e22bf33885a7fb2c266e411bc
Sarthak Singh <sarthaksingh2010@gmail.com>
2019-03-13 19:13:20 +0530

    Create Accuracy Calculate Random Forest.py

---
+++ MachineLearning Work Space/Accuracy Calculate Random Forest.py
@@ -0,0 +1,16 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score
+
+wine=datasets.load_wine()
+print(wine.feature_names)
+print(wine.data[0])
+print(wine.target[0])
+x=wine.data
+y=wine.target
+x_train,x_test,y_train,y_test=tts(x,y,test_size=0.2)
+clf=RandomForestClassifier()
+clf.fit(x_train,y_train)
+predictions=clf.predict(x_test)
+print(accuracy_score(y_test,predictions))
